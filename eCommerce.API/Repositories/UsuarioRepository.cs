eb3fd039cb2f0074c41795a7ad86efc0637e9a8f
Samuel Monteiro <ss-monteiro@outlook.com>
2025-03-10 22:34:28 -0400

    Aula 35 concluída - Atualizando banco e testando conexão via postman

--- eCommerce.API/Repositories/UsuarioRepository.cs
+++ eCommerce.API/Repositories/UsuarioRepository.cs
@@ -1,34 +1,42 @@
-﻿using eCommerce.Models;
+﻿using eCommerce.API.Database;
+using eCommerce.Models;
 
 namespace eCommerce.API.Repositories
 {
     public class UsuarioRepository : IUsuarioRepository
     {
-        public static List<Usuario> _db = new List<Usuario>();
+        private readonly eCommerceContext _db;
+        public UsuarioRepository(eCommerceContext db)
+        {
+            _db = db;
+        }
+
         public void Add(Usuario usuario)
         {
             _db.Add(usuario);
+            _db.SaveChanges();
         }
 
         public void Delete(int id)
         {
-            _db.Remove(Get(id));
+            _db.Usuarios.Remove(Get(id));
+            _db.SaveChanges();
         }
 
         public List<Usuario> Get()
         {
-            return _db;
+            return _db.Usuarios.OrderBy(a => a.Id).ToList();
         }
 
         public Usuario Get(int id)
         {
-            return _db.Find(x => x.Id == id)!;
+            return _db.Usuarios.Find(id)!;
         }
 
         public void Update(Usuario usuario)
         {
-            _db.Remove(Get(usuario.Id));
-            _db.Add(usuario);
+            _db.Update(usuario.Id);
+            _db.SaveChanges();
         }
     }
 }
